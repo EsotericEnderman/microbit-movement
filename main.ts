718b575ec3230be1fb58880e2998aa9b384d7860
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-02 13:54:02 +0100

    Implement movement system

---
+++ main.ts
@@ -0,0 +1,52 @@
+enum MovementDirection {
+    UP = 0,
+    RIGHT = 1,
+    DOWN = 2,
+    LEFT = 3
+}
+
+class MovingSquare {
+
+    private movementDirection: MovementDirection = MovementDirection.UP;
+
+    private x: number;
+    private y: number;
+
+    constructor(startingX: number, startingY: number) {
+        this.x = startingX;
+        this.y = startingY;
+
+        this.move(this.x, this.y);
+    }
+
+    private redraw() {
+        basic.clearScreen();
+        led.plot(this.x, this.y);
+    }
+
+    private move(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+
+        this.redraw();
+    }
+
+    public onButtonAPressed() {
+        this.movementDirection = (this.movementDirection + 1) % 4
+    }
+
+    public onButtonBPressed() {
+        const angleDegrees = 90.0 - this.movementDirection * 90;
+        const angleRadians = angleDegrees * Math.PI / 180.0;
+
+        const xMovement = Math.round(Math.cos(angleRadians));
+        const yMovement = Math.round(Math.sin(angleRadians));
+
+        this.move(this.x + xMovement, this.y + yMovement);
+    }
+}
+
+const movingSquare = new MovingSquare(2, 2);
+
+input.onButtonPressed(Button.A, () => movingSquare.onButtonAPressed());
+input.onButtonPressed(Button.B, () => movingSquare.onButtonBPressed());
